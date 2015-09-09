e682a9392b6deee58cb6bbab6f81679e26514d6d
Charl <charl.thiem@gmail.com>
2015-09-09 21:00:20 +0200

    Converted "push" pages to proper angular pages - work in progress

---
+++ ui/src/main/webapp/js/push/push.routes.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Charl on 2015-09-09.
+ */
+;(function(angular){
+	angular.module('kme-push', ['ngRoute'])
+		.config(['$routeProvider',
+			function($routeProvider) {
+				$routeProvider
+					.when('/', 			{templateUrl: contextPath + 'html/push/menu.html'})
+					.when('/send',	{templateUrl: contextPath + 'html/push/send.html'})
+					.when('/history',		{templateUrl: contextPath + 'html/push/history.html'})
+					.when('/queue',		{templateUrl: contextPath + 'html/push/queue.html'})
+					.otherwise({ redirectTo: '/' });
+			}]);
+})(angular);
